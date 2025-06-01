0bc3edd1fdb1466f5319f3227f0567f513856528
raaflii <rafliwrzky@gmail.com>
2025-06-01 21:50:35 +0700

    remove welcome template form laravel

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Route::get('/landing-page', function () {
     return view('landing-page');
