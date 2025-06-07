1b97c76817c3445fac3af5cd739a5e89647db473
raaflii <rafliwrzky@gmail.com>
2025-06-07 22:02:26 +0700

    update route for news

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\NewsController;
 
 
 Route::get('/landing-page', function () {
     return view('landing-page');
 });
 
+Route::prefix('admin')->group(function () {
+    Route::resource('news', NewsController::class);
+});
